8062afd47418b9a02ac874d1a4fe4dc4be01a396
DrizzyJ56 <blevins.j921@gmail.com>
2023-02-15 11:04:21 -0500

    first commit/Created table on Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import dummyContacts from "../dummyData";
 
 const Main = () => {
+  const [contact, setContacts] = useState(dummyContacts);
+
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+            </tr>
+              {contact.map((e) => {
+              return <tr key={e.id}>
+                <td>{e.name}</td>
+                <td>{e.phone}</td>
+                <td>{e.email}</td>
+              </tr>
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
